fa96722299fec4b6c821bb8e45dd89ce08dac65b
zvup <121556153+zvup@users.noreply.github.com>
2024-07-28 15:23:39 +0200

    removed unused logging impl

--- logging/logging.go
+++ logging/logging.go
@@ -1,13 +1,8 @@
 package logging
 
 import (
-	"bytes"
-	"fmt"
-	"log"
-	"os"
-	"sync"
-
 	"github.com/fatih/color"
+	"log"
 )
 
 type Logger interface {
@@ -16,33 +11,10 @@ type Logger interface {
 	Info(a ...interface{})
 	Fatal(a ...interface{})
 }
-type ColorLogger struct {
-	green  func(a ...interface{}) string
-	red    func(a ...interface{}) string
-	msgBuf bytes.Buffer
-	file   *os.File
-	mu     sync.Mutex
-	writes int
-}
+
 type LoggerWithoutFile struct {
 	green func(a ...interface{}) string
 	red   func(a ...interface{}) string
-}
-
-func (l *ColorLogger) writeToFile() error {
-	if l.file == nil {
-		return fmt.Errorf("file does not exist")
-	}
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	content := l.msgBuf.String()
-	_, err := l.file.Write([]byte(content))
-	if err != nil {
-		return err
-	}
-
-	l.msgBuf.Reset()
-	return nil
 }
 
 func NewLoggerWithoutFile() Logger {
